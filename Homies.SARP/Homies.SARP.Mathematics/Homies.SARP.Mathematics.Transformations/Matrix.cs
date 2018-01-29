b76cc3b2453d4793da198bb57d90a0a1339acc4d
Denis Jay <denisjuschanin@gmail.com>
2018-01-29 17:59:35 +0100

    Initially setup for all dem homies. Rules of the homies:  - No bitching  - No fighting  - No ternary expressions  - No inline if-statements (if you know what's good for you)

---
+++ Homies.SARP/Homies.SARP.Mathematics/Homies.SARP.Mathematics.Transformations/Matrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Media.Media3D;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Homies.SARP.Mathematics.Transformations
+{
+	public class Matrix
+	{
+		#region FIELDS
+		DenseMatrix _denseMatrix;
+		Matrix3D _matrix3D;
+		#endregion
+
+		#region PROPERTIES
+		public DenseMatrix DenseMatrix { get => _denseMatrix; set => _denseMatrix = value; }
+		public Matrix3D Matrix3D { get => _matrix3D; set => _matrix3D = value; }
+		#endregion
+
+
+		public Matrix(DenseMatrix matrix = null)
+		{
+			DenseMatrix = matrix;
+		}
+
+		public Matrix3D GetMatrix3D()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string ToString()
+		{
+			if (DenseMatrix !=null)
+			{
+				return DenseMatrix.ToString();
+			}
+
+			return ("DenseMatrix is null\t" + base.ToString());
+		}
+
+		public override bool Equals(object obj)
+		{
+			var mat = obj as Matrix;
+			if (mat != null)
+			{
+				return DenseMatrix.Equals(mat.DenseMatrix);
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+}
---
+++ Homies.SARP/Homies.SARP.Mathematics/Homies.SARP.Mathematics.Transformations/Transformations.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Homies.SARP.Mathematics.Transformations
+{
+	public static class Transformations
+	{
+		/// <summary>
+		/// Returns a rotation matrix rotated around the x axis
+		/// </summary>
+		/// <param name="thetaRad">rotation angle specified in radians</param>
+		/// <returns></returns>
+		public static DenseMatrix GetRotMatrixX(double thetaRad)
+		{
+			var rotX = DenseMatrix.OfArray(new double[,]
+			{
+				{ 1, 0, 0, 0},
+				{ 0, Math.Cos(thetaRad), -Math.Sin(thetaRad), 0},
+				{ 0, Math.Sin(thetaRad), Math.Cos(thetaRad), 0},
+				{ 0, 0, 0, 1 }
+			});
+			rotX.CoerceZero(0.000001);
+			return rotX;
+		}
+
+		/// <summary>
+		/// Returns a rotation matrix rotated around the y axis
+		/// </summary>
+		/// <param name="thetaRad">rotation angle specified in radians</param>
+		/// <returns></returns>
+		public static DenseMatrix GetRotMatrixY(double thetaRad)
+		{
+			var rotY = DenseMatrix.OfArray(new double[,]
+			{
+				{ Math.Cos(thetaRad), 0, Math.Sin(thetaRad), 0},
+				{ 0, 1, 0, 0},
+				{ -Math.Sin(thetaRad), 0, Math.Cos(thetaRad), 0},
+				{ 0, 0, 0, 1 }
+			});
+			rotY.CoerceZero(0.000001);
+			return rotY;
+		}
+
+		/// <summary>
+		/// Returns a rotation matrix rotated around the z axis
+		/// </summary>
+		/// <param name="thetaRad">rotation angle specified in radians</param>
+		/// <returns></returns>
+		public static DenseMatrix GetRotMatrixZ(double thetaRad)
+		{
+			var rotZ = DenseMatrix.OfArray(new double[,]
+			{
+				{ Math.Cos(thetaRad), -Math.Sin(thetaRad), 0, 0},
+				{ Math.Sin(thetaRad), Math.Cos(thetaRad), 0, 0},
+				{ 0, 0, 1, 0},
+				{ 0, 0, 0, 1 }
+			});
+			rotZ.CoerceZero(0.000001);
+			return rotZ;
+		}
+
+		/// <summary>
+		/// Returns a translation matrix
+		/// </summary>
+		/// <param name="x">translates in x direction</param>
+		/// <param name="y">translates in y direction</param>
+		/// <param name="z">translates in z direction</param>
+		/// <returns></returns>
+		public static DenseMatrix GetTranslationMatrix(double x, double y, double z)
+		{
+			var trans = DenseMatrix.OfArray(new double[,]
+			{
+				{ 1, 0, 0, x},
+				{ 0, 1, 0, y},
+				{ 0, 0, 1, z},
+				{ 0, 0, 0, 1 }
+			});
+			trans.CoerceZero(0.000001);
+			return trans;
+		}
+	}
+}
