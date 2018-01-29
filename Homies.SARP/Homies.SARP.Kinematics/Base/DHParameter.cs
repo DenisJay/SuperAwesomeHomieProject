b729f74c98fe8c1a873aa52175ccf72fd50cd137
Denis Juschanin <denisjuschanin@gmail.com>
2018-01-29 21:13:32 +0100

    added structure for robot joints and DHParameter.

---
+++ Homies.SARP/Homies.SARP.Kinematics/Base/DHParameter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homies.SARP.Kinematics.Base
+{
+	public class DHParameter
+	{
+
+		#region FIELDS
+
+		double _a;
+		double _alpha;
+		double _d;
+		double _theta;
+
+		#endregion //FIELDS
+
+		#region PROPERTIES
+
+		public double A { get => _a; private set => _a = value; }
+		public double Alpha { get => _alpha; private set => _alpha = value; }
+		public double D { get => _d; set => _d = value; }
+		public double Theta { get => _theta; set => _theta = value; }
+
+		#endregion //PROPERTIES
+
+
+		public DHParameter(double a, double d, double alpha, double theta)
+		{
+			A = a;
+			D = d;
+			Alpha = alpha;
+			Theta = theta;
+		}
+
+
+	}
+}
---
+++ Homies.SARP/Homies.SARP.Kinematics/Base/RotationalJoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Homies.SARP.Kinematics.Base
+{
+	public class RotationalJoint
+	{
+
+	}
+}
