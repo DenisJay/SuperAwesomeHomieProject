73d250932612230130e9ca2adcac87de43a55251
Denis Juschanin <denis.juschanin@thyssenkrupp.com>
2018-02-09 06:31:41 +0100

    blablablabla and some other stuff

---
+++ Homies.SARP/Homies.SARP.Machines/Homies.SARP.Machines.MachineStructures/Robot.cs
@@ -0,0 +1,41 @@
+﻿using Homies.SARP.Machines.BaseStructure;
+using Homies.SARP.Mathematics.Transformations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Homies.SARP.Machines.MachineStructures
+{
+	public class Robot
+	{
+		string _modelName;
+		SortedList<int, Joint> _joints;
+
+		public string ModelName
+		{
+			get { return _modelName; }
+			set { _modelName = value; }
+		}
+
+		public SortedList<int, Joint> Joints
+		{
+			get { return _joints; }
+			set { _joints = value; }
+		}
+
+		public TransformationMatrix TCP { get; set; }
+
+		public Robot(string name, List<DHParameter> dhParam)
+		{
+			Joints = new SortedList<int, Joint>();
+			ModelName = name;
+
+			for (int i = 0; i < dhParam.Count; i++)
+			{
+				Joints.Add(i, new RotationalJoint(0, 360, dhParam[i]));
+			}
+		}
+	}
+}
