692bb49f4e4ab188484f6634adac53fcaacd1ce4
Denis Jay <denisjuschanin@gmail.com>
2018-02-17 22:55:12 +0100

    Started implementing computation of angle 2 and angle 3.

--- Homies.SARP/Homies.SARP.Kinematics/Homies.SARP.Kinematics.Inverse/InverseKinematics.cs
+++ Homies.SARP/Homies.SARP.Kinematics/Homies.SARP.Kinematics.Inverse/InverseKinematics.cs
@@ -15,26 +15,66 @@ namespace Homies.SARP.Kinematics.Homies.SARP.Kinematics.Inverse
 	// 3. When turn / status is specified choose correct angle
 	public class InverseKinematics
 	{
-		public List<double> ResultAxisValues { get; set; }
-		
+
+		public List<double[]> ResultAxisSolutions { get; private set; }
+		public List<double> ResultAxisValues { get; private set; }
+
+		public InverseKinematics()
+		{
+			ResultAxisSolutions = new List<double[]>();
+			ResultAxisValues = new List<double>();
+		}
+
 		public List<double> GetInverseKinematics(TransformationMatrix targetMatrix, List<DHParameter> dhParam)
 		{
-			List<double> returnAngles = new List<double>();
+			List<double> returnAnglesForTest = new List<double>();
 
-			TransformationMatrix wristToAxis1 = new TransformationMatrix(
+			ComputeAngle1Solutions(targetMatrix, dhParam);
+			//ComputeAngle23Solutions(targetMatrix, dhParam);
+
+			returnAnglesForTest.Add(ResultAxisSolutions[0][0]);
+			
+
+			//TODO: Implement "ChooseValidConfiguration()"
+			//ChooseValidConfiguration()
+
+			return returnAnglesForTest;
+		}
+
+		/// <summary>
+		/// Very easy. look at the wrist point in the xy plane starting from standard axis 1 transformation
+		/// (theta1 angle is neglected in order to get the sign correct. 
+		/// Interesting for kuka since they have this weird convention where the rotation direction vector of the axis 1 is pointing downwards)
+		/// </summary>
+		/// <param name="targetMatrix">The target frame in some workspace</param>
+		/// <param name="dhParam">the dhparameter of the kinematic chain</param>
+		private void ComputeAngle1Solutions(TransformationMatrix targetMatrix, List<DHParameter> dhParam)
+		{
+			TransformationMatrix wristToAxis1 = new TransformationMatrix( 
 				(DenseMatrix)dhParam.First().JointStandardTransform.DenseMatrix.Inverse() * 
 				targetMatrix.DenseMatrix * 
 				(DenseMatrix)dhParam.Last().JointTransform.DenseMatrix.Inverse());
 
 			double angleDeg1Plus = Math.Atan2(wristToAxis1.Matrix3D.OffsetY, wristToAxis1.Matrix3D.OffsetX) * 180 / Math.PI;
 			double angleDeg1Minus = Math.Atan2(-wristToAxis1.Matrix3D.OffsetY, -wristToAxis1.Matrix3D.OffsetX) * 180 / Math.PI;
+			ResultAxisSolutions.Add(new double[] { angleDeg1Plus, angleDeg1Minus });
+		}
 
-			
 
-			returnAngles.Add(angleDeg1Plus);
-			returnAngles.Add(angleDeg1Minus);
+		private void ComputeAngle23Solutions(TransformationMatrix targetMatrix, List<DHParameter> dhParam)
+		{
+			var rootToAxis2Trans = new TransformationMatrix(dhParam.First().JointTransform.DenseMatrix * dhParam[1].JointStandardTransform.DenseMatrix);
 
-			return returnAngles;
+			TransformationMatrix wristToAxis2 = new TransformationMatrix( 
+				(DenseMatrix)rootToAxis2Trans.DenseMatrix.Inverse() * 
+				targetMatrix.DenseMatrix * 
+				(DenseMatrix)dhParam.Last().JointTransform.DenseMatrix.Inverse());
+
+			Debug.Print("\n" + wristToAxis2);
+
+			Console.WriteLine("Do some computation here");
+
+			throw new NotImplementedException();
 		}
 	}
 }
