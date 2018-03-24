42bea450db945285816706a21c5d27bf8b5d1331
Denis Jay <denisjuschanin@gmail.com>
2018-03-24 20:45:48 +0100

    Added Class and enum regarding the robot status for a defined position.

---
+++ Homies.SARP/Homies.SARP.Kinematics/Homies.SARP.Kinematics.Common/RobotPoseStatusConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Homies.SARP.Kinematics.Enums;
+
+namespace Homies.SARP.Kinematics.Homies.SARP.Kinematics.Common
+{
+	public class RobotPoseStatusConfiguration
+	{
+		public RobotPoseStatus ForwardBackup { get; set; }
+		public RobotPoseStatus Elbow { get; set; }
+		public RobotPoseStatus Wrist { get; set; }
+
+		public RobotPoseStatusConfiguration(RobotPoseStatus forwardBackup, RobotPoseStatus elbow, RobotPoseStatus wrist)
+		{
+			ForwardBackup = forwardBackup;
+			Elbow = elbow;
+			Wrist = wrist;
+
+		}
+	}
+}
---
+++ Homies.SARP/Homies.SARP.Kinematics/Homies.SARP.Kinematics.Enums/RobotPoseStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Homies.SARP.Kinematics.Enums
+{
+	public enum RobotPoseStatus
+	{
+		FORWARD, BACKWARD,
+		ELBOW_UP, ELBOW_DOWN,
+		WRIST_NORMAL, WRIST_FLIPPED
+	}
+}
